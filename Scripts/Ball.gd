91fd8c312360878e90f0be2dca1208b3530c846f
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-02 11:37:46 -0400

    Update Ball.gd

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -6,18 +6,17 @@ func _process(delta):
 	if Input.is_action_just_pressed("ClickL"):
 		if sleeping:
 			leap()
-	_draw()
-
-func _draw():
-	draw_line(position, get_global_mouse_position(), Color(255, 255, 255))
 
 func leap():
 	sleeping = false
 	var Velocity = get_local_mouse_position()
 	apply_impulse(Velocity, Velocity)
+	print(Velocity)
 
 func _on_DetectCollision_body_entered(body):
 	if body.get_parent().name == "Platforms":
 		linear_velocity = Vector2(0, 0)
+		angular_velocity = 0
+		applied_force = Vector2(0, 0)
+		linear_velocity = Vector2(0, 0)
 		sleeping = true
-		print(angular_velocity)
