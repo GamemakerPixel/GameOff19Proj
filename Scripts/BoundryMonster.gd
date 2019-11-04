14eafa6c7683ff4895f4a63d07a5823da5e87fc0
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-04 10:40:18 -0500

    Create BoundryMonster.gd

---
+++ Scripts/BoundryMonster.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+export (int) var startSpeed = 10
+var speed
+var ready
+
+func _ready():
+	speed = startSpeed
+	ready = false
+
+func _process(delta):
+	if ready:
+		move_and_slide(Vector2(speed, 0))
+		speed += 0.3
+
+func _on_Collider_body_entered(body):
+	if body.name == "Ball":
+		get_tree().reload_current_scene()
