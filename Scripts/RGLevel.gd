b41bf94b7f84e62eb26f8e3f064627abe8c1b341
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-06 08:34:17 -0500

    Create RGLevel.gd

---
+++ Scripts/RGLevel.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+export (int) var distance = 10000
+export (int) var platformDisBetween = 500
+export (int) var percentPlatformFrequency = 100
+
+var posQueue = [0]
+
+func _ready():
+	print("READY")
+	runGenerationSequence()
+
+func runGenerationSequence():
+	while posQueue.back() != platformDisBetween:
+		if posQueue.back() == 0:
+			posQueue.remove(posQueue.find(0))
+		var value
+		if posQueue.size() > 0:
+			value = posQueue.back() - platformDisBetween
+		else:
+			value = distance - platformDisBetween
+		posQueue.append(value)
+	for pos in posQueue:
+		print(pos)
+		generateForPos(pos)
+		generateForPos(pos + platformDisBetween/2, "top")
+
+func generateForPos(pos, height = "low"):
+	print("Generate for pos " + str(pos))
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	if rng.randi_range(1, 100) <= percentPlatformFrequency:
+		var object = preload("res://GameOff19Proj/Platforms/Normal.tscn").instance()
+		$Platforms.add_child(object)
+		object.position = Vector2(pos, 504)
+		object.scale.x = rng.randi_range(2, 7)
+		object.rotation_degrees = rng.randi_range(-10, 10)
+		print("Place " + str(object) + " at pos " + str(pos))
