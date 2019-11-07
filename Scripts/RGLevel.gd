c1ec769383ec68b4fe45e1c6cff4eb017fe5d920
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-07 17:11:25 -0500

    Update RGLevel.gd

--- Scripts/RGLevel.gd
+++ Scripts/RGLevel.gd
@@ -1,32 +1,36 @@
 extends Node2D
 
-export (int) var distance = 10000
+export (int) var chunkDistance = 10000
 export (int) var platformDisBetween = 500
 export (int) var percentPlatformFrequency = 100
-
-var posQueue = [0]
+var posQueue = [-1]
+var currentChunkReach = 0
 
 func _ready():
-	print("READY")
-	runGenerationSequence()
+	runGenerationSequence(0, chunkDistance)
 
-func runGenerationSequence():
+func runGenerationSequence(from, to):
+	print("Generating from " + str(from) + " to " + str(to))
+	currentChunkReach = to
+	placeBoundries(from, to)
 	while posQueue.back() != platformDisBetween:
-		if posQueue.back() == 0:
-			posQueue.remove(posQueue.find(0))
+		if posQueue.back() == -1:
+			posQueue.clear()
 		var value
 		if posQueue.size() > 0:
 			value = posQueue.back() - platformDisBetween
 		else:
-			value = distance - platformDisBetween
-		posQueue.append(value)
+			value = to - platformDisBetween
+		if value >= from && value <= to:
+			posQueue.append(value)
+		else:
+			break
 	for pos in posQueue:
 		print(pos)
 		generateForPos(pos)
 		generateForPos(pos + platformDisBetween/2, "top")
 
 func generateForPos(pos, height = "low"):
-	print("Generate for pos " + str(pos))
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	if rng.randi_range(1, 100) <= percentPlatformFrequency:
@@ -38,4 +42,18 @@ func generateForPos(pos, height = "low"):
 			object.position = Vector2(pos, 504)
 		object.scale.x = rng.randi_range(5, 9)
 		object.rotation_degrees = rng.randi_range(-10, 10)
-		print("Place " + str(object) + " at pos " + str(pos))
+
+func placeBoundries(from, to):
+	var b1 = preload("res://GameOff19Proj/Boundries/BoundryFluid.tscn").instance()
+	var b2 = preload("res://GameOff19Proj/Boundries/BoundryFluid.tscn").instance()
+	b1.position = Vector2(to/2, 604)
+	b2.position = Vector2(to/2, 904)
+	b1.scale.x = chunkDistance/960
+	b2.scale = b1.scale
+	add
+
+func _process(delta):
+	if $Characters/Ball.position.x >= currentChunkReach - 2000:
+		print("Preparing to generate from " + str(currentChunkReach) + " to " + str(currentChunkReach + chunkDistance))
+		posQueue.append(-1)
+		runGenerationSequence(currentChunkReach, currentChunkReach + chunkDistance)
