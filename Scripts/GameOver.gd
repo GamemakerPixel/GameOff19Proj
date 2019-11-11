be5114ba06a20af93b28ac0022b5917356f5ede9
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-11 10:37:28 -0500

    Update GameOver.gd

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,13 +1,16 @@
 extends ColorRect
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func appear(score):
+	$AnimationPlayer.play("GameOver")
+	$Score.text = "Score: " + str(score)
+
+func _on_Retry_pressed():
+	get_tree().reload_current_scene()
+
+func _on_Menu_pressed():
+	print("MenuUnavaliable")
+
+func _on_Quit_pressed():
+	get_tree().quit()
