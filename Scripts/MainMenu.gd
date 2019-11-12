a9cbeb6b0828c30d262af0583dfc62e174561686
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-12 12:11:19 -0500

    Create MainMenu.gd

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_Play_pressed():
+	pass # Replace with function body.
+
+func _on_Modes_pressed():
+	pass # Replace with function body.
+
+func _on_Metals_pressed():
+	pass # Replace with function body.
+
+func _on_Custom_pressed():
+	pass # Replace with function body.
+
+func _on_Quit_pressed():
+	get_tree().quit()
