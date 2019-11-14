923d018870cd47074e7e5a47b6e2712bb33a6fc7
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-14 11:59:17 -0500

    Update GameOver.gd

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -10,7 +10,7 @@ func _on_Retry_pressed():
 	get_tree().reload_current_scene()
 
 func _on_Menu_pressed():
-	print("MenuUnavaliable")
+	get_tree().change_scene("res://GameOff19Proj/Menus/MainMenu.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
