62c9277e20b4897d540fb36cc7097647e37f45af
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-13 19:59:28 -0500

    Update BoundryFluid.gd

--- Scripts/BoundryFluid.gd
+++ Scripts/BoundryFluid.gd
@@ -2,7 +2,7 @@ extends AnimatedSprite
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Ball":
-		body.position.x -= 1000
+		body.position.x -= 500
 		if body.position.x < 400:
 			body.position.x = 400
 		body.position.y = 654
