3a755ea09b756f39e09f5c324de6c5ee7f5db877
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-13 19:57:49 -0500

    Update GlobalVariables.gd

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -1,4 +1,4 @@
 extends Node
 
-var gameGenerationSettings = [600, 80, 4, 20, 50, 0.6] #[500, 90, 5, 10, 10, 0.3] [550, 85, 4.5, 15, 30, 0.4] [600, 80, 4, 20, 50, 0.6]
+var gameGenerationSettings = [500, 90, 5, 10, 10, 0.3] #[500, 90, 5, 10, 10, 0.3] [550, 85, 4.5, 15, 30, 0.4] [600, 80, 4, 20, 50, 0.6]
 var medalsUnlocked = [false, false, false, false, false, false, false, false, false, false]
