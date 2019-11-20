e73cd759872b5af4be680b0748885a743830db26
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-20 07:31:09 -0500

    Create CustomGame.gd

---
+++ Scripts/CustomGame.gd
@@ -0,0 +1,42 @@
+extends Control
+
+var dbp = GlobalVariables.gameGenerationSettings[0]
+var pnsp = GlobalVariables.gameGenerationSettings[1]
+var mps = GlobalVariables.gameGenerationSettings[2]
+var pmr = GlobalVariables.gameGenerationSettings[3]
+var bss = GlobalVariables.gameGenerationSettings[4]
+var ba = GlobalVariables.gameGenerationSettings[5]
+
+func _ready():
+	setValue("DBP", dbp / 10, 0, 10)
+	setValue("PnSP", -pnsp + 100, 1)
+	setValue("MPS", mps, 2)
+	setValue("PMR", pmr, 3)
+	setValue("BSS", bss, 4)
+	setValue("BA", ba / 0.1, 5, 0.1)
+
+func setValue(slider, value, settingPosition, multiplier = 1):
+	var sliderNode = get_node("VBoxContainer/" + slider)
+	var valueLabel = sliderNode.get_node("Value")
+	#sliderNode.value = value
+	valueLabel.text = str(value * multiplier)
+	var trueValue = value * multiplier
+	GlobalVariables.gameGenerationSettings[settingPosition] = trueValue
+
+func _on_DBP_value_changed(value):
+	setValue("DBP", value, 0, 10)
+
+func _on_PnSP_value_changed(value):
+	setValue("PnSP", -value + 100, 1)
+
+func _on_MPS_value_changed(value):
+	setValue("MPS", value, 2)
+
+func _on_PMR_value_changed(value):
+	setValue("PMR", value, 3)
+
+func _on_BSS_value_changed(value):
+	setValue("BSS", value, 4)
+
+func _on_BA_value_changed(value):
+	setValue("BA", value, 5, 0.1)
