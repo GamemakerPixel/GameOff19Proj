0de4a4966496d61d45447c40e129f88eda0adc89
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-21 07:20:46 -0500

    Update MainMenu.gd

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,6 +2,9 @@ extends Control
 
 
 
+func _ready():
+	$AnimationPlayer.play("mainmenu")
+
 func _on_Play_pressed():
 	get_tree().change_scene("res://GameOff19Proj/Levels/RGLevel.tscn")
 
@@ -12,7 +15,7 @@ func _on_Metals_pressed():
 	get_tree().change_scene("res://GameOff19Proj/Menus/Medals.tscn")
 
 func _on_Custom_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://GameOff19Proj/Menus/CustomGame.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
