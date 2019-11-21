2e1748c857ebb355241beed22200bf7d6681c703
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-21 07:20:40 -0500

    Update CustomGame.gd

--- Scripts/CustomGame.gd
+++ Scripts/CustomGame.gd
@@ -18,16 +18,19 @@ func _ready():
 func setValue(slider, value, settingPosition, multiplier = 1):
 	var sliderNode = get_node("VBoxContainer/" + slider)
 	var valueLabel = sliderNode.get_node("Value")
-	#sliderNode.value = value
+	sliderNode.value = value
 	valueLabel.text = str(value * multiplier)
 	var trueValue = value * multiplier
-	GlobalVariables.gameGenerationSettings[settingPosition] = trueValue
+	if slider == "PnSP":
+		GlobalVariables.gameGenerationSettings[settingPosition] = 100 - trueValue
+	else:
+		GlobalVariables.gameGenerationSettings[settingPosition] = trueValue
 
 func _on_DBP_value_changed(value):
 	setValue("DBP", value, 0, 10)
 
 func _on_PnSP_value_changed(value):
-	setValue("PnSP", -value + 100, 1)
+	setValue("PnSP", value, 1)
 
 func _on_MPS_value_changed(value):
 	setValue("MPS", value, 2)
@@ -40,3 +43,9 @@ func _on_BSS_value_changed(value):
 
 func _on_BA_value_changed(value):
 	setValue("BA", value, 5, 0.1)
+
+func _on_Back_pressed():
+	get_tree().change_scene("res://GameOff19Proj/Menus/MainMenu.tscn")
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://GameOff19Proj/Levels/RGLevel.tscn")
