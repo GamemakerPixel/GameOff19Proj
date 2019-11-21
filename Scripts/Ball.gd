3cc48e60eb6d35161d047744fcb30ad1aadc34a2
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-21 07:20:25 -0500

    Update Ball.gd

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -11,8 +11,6 @@ func _process(delta):
 	if startSleeping:
 		sleeping = true
 		startSleeping = false
-	if Input.is_action_pressed("ui_up"):
-		position.x += 1000
 	score = int(position.x / 50) - 8
 	$CanvasLayer/Score.text = "Score: " + str(score)
 	if Input.is_action_just_pressed("ClickL"):
@@ -27,11 +25,10 @@ func _process(delta):
 		$Camera2D.shake(0.5, 20, (40 - ((position.x - get_parent().get_node("BoundryMonster").position.x)/100))/4)
 		if position.x - get_parent().get_node("BoundryMonster").position.x < 2550:
 			var opacity = (2550 - (position.x - get_parent().get_node("BoundryMonster").position.x)) / 10
-			print(opacity)
 			$CanvasLayer/ColorRect.color = Color8(255, 15, 15, opacity)
 		else:
 			$CanvasLayer/ColorRect.color = Color8(255, 15, 15, 0)
-	if position.y > 904 || position.y < 404:
+	if position.y > 904 || position.y < 204:
 		shortReset()
 	determineMedals()
 
@@ -48,63 +45,63 @@ func determineMedals():
 		if score >= 300:
 			if not GlobalVariables.medalsUnlocked[0]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(0, 0)
-				$CanvasLayer/Medal/Label.text = "Easy - Bronze"
+				$CanvasLayer/Medal/Label.text = "Easy - 300"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[0] = true
 		if score >= 500:
 			if not GlobalVariables.medalsUnlocked[1]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(16, 0)
-				$CanvasLayer/Medal/Label.text = "Easy - Silver"
+				$CanvasLayer/Medal/Label.text = "Easy - 500"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[1] = true
 		if score >= 700:
 			if not GlobalVariables.medalsUnlocked[2]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(32, 0)
-				$CanvasLayer/Medal/Label.text = "Easy - Gold"
+				$CanvasLayer/Medal/Label.text = "Easy - 700"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[2] = true
 	if GlobalVariables.gameGenerationSettings == [550, 85, 4.5, 15, 30, 0.4]:
 		if score >= 300:
 			if not GlobalVariables.medalsUnlocked[3]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(0, 16)
-				$CanvasLayer/Medal/Label.text = "Medium - Bronze"
+				$CanvasLayer/Medal/Label.text = "Medium - 300"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[3] = true
 		if score >= 500:
 			if not GlobalVariables.medalsUnlocked[4]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(16, 16)
-				$CanvasLayer/Medal/Label.text = "Medium - Silver"
+				$CanvasLayer/Medal/Label.text = "Medium - 500"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[4] = true
 		if score >= 700:
 			if not GlobalVariables.medalsUnlocked[5]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(32, 16)
-				$CanvasLayer/Medal/Label.text = "Medium - Gold"
+				$CanvasLayer/Medal/Label.text = "Medium - 700"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[5] = true
 	if GlobalVariables.gameGenerationSettings == [600, 80, 4, 20, 50, 0.6]:
 		if score >= 300:
 			if not GlobalVariables.medalsUnlocked[6]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(0, 32)
-				$CanvasLayer/Medal/Label.text = "Hard - Bronze"
+				$CanvasLayer/Medal/Label.text = "Hard - 300"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[6] = true
 		if score >= 500:
 			if not GlobalVariables.medalsUnlocked[7]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(16, 32)
-				$CanvasLayer/Medal/Label.text = "Hard - Silver"
+				$CanvasLayer/Medal/Label.text = "Hard - 500"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[7] = true
 		if score >= 700:
 			if not GlobalVariables.medalsUnlocked[8]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(32, 32)
-				$CanvasLayer/Medal/Label.text = "Hard - Gold"
+				$CanvasLayer/Medal/Label.text = "Hard - 700"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[8] = true
 		if score >= 1000:
 			if not GlobalVariables.medalsUnlocked[9]:
 				$CanvasLayer/Medal.region_rect.position = Vector2(0, 48)
-				$CanvasLayer/Medal/Label.text = "PLATINUM"
+				$CanvasLayer/Medal/Label.text = "PLATINUM - Hard - 1000"
 				$CanvasLayer/Medal/AnimationPlayer.play("Medal")
 				GlobalVariables.medalsUnlocked[9] = true
 
@@ -114,7 +111,6 @@ func update_visuals():
 		$CanvasLayer/Tracker.scale = Vector2(1000/monToSelfDis, 1000/monToSelfDis)
 
 func leap():
-	print("LEAP")
 	var starting = true
 	if starting:
 		get_parent().get_node("BoundryMonster").ready = true
