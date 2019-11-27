d43130e295712168c815402e9260c0522ec4ebb6
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-27 09:53:03 -0500

    Update GameOver.gd

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -10,7 +10,7 @@ func disappear():
 	self.hide()
 
 func _on_Retry_pressed():
-	get_tree().reload_current_scene()
+	get_parent().get_parent().get_parent().get_parent().reset()
 
 func _on_Menu_pressed():
 	get_tree().change_scene("res://GameOff19Proj/Menus/MainMenu.tscn")
