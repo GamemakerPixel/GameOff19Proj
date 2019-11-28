5fad78132523388bc0d5376d2c4bcdc0301e66f0
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-28 09:55:04 -0500

    Update GameOver.gd

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -5,11 +5,13 @@ extends ColorRect
 func appear(score):
 	$AnimationPlayer.play("GameOver")
 	$Score.text = "Score: " + str(score)
+	get_parent().get_parent().onGameOver = true
 
 func disappear():
-	self.hide()
+	get_tree().reload_current_scene()
 
 func _on_Retry_pressed():
+	disappear()
 	get_parent().get_parent().get_parent().get_parent().reset()
 
 func _on_Menu_pressed():
